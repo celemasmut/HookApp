98aecd8ac266a74122b5d537b7b69d780ed9aec1
Celeste Masmut <celeste.masmut.ext@pedidosya.com>
2022-01-13 01:44:43 -0300

    - reset

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './counter.css'
+import { useCounter } from '../../hooks/useCounter'
 
-const CounterWithCustomHook = props => {
+
+const CounterWithCustomHook = () => {
+
+
+
+    const { state, increment, decrement, reset } = useCounter(100);
+
     return (
         <>
-        <h1>Counter with hook: { 0 } </h1>
+        <h1>Counter with hook: { state } </h1>
         <hr />
 
-        <button className="btn"> +1 </button>    
-        <button className="btn"> -1 </button>    
+        <button onClick={ () => increment(2)} className="btn"> +1 </button>    
+        <button onClick={ ()=> decrement(2)} className="btn"> -1 </button>    
+        <button onClick={ reset} className="btn"> reset </button>    
+
         </>
     )
 }
