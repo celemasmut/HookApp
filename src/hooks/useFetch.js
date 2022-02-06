a17e5c1168512c4d779a3be1cb9878b19b828d19
Celeste Masmut <celeste.masmut.ext@pedidosya.com>
2022-02-06 17:06:23 -0300

    useRef

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,16 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export const useFetch = (url) => {
 
+    const isMounted = useRef(true)
+
     const [state, setstate] = useState({data: null, loading: true, error: null});
+
+    useEffect ( ()=>{
+        return () => {
+            isMounted.current = false;
+        }
+    }, [] )
 
 
     //se ejecuta cuando el url cambia 
@@ -14,11 +22,15 @@ export const useFetch = (url) => {
         fetch(url)
             .then( resp => resp.json() ) // de la promesa se estrae la respuesta lo cual regresa otra promesa
             .then( data => {// se obtiene la data de la promesa y se setea los cambios
-                setstate({
-                    loading: false,
-                    error: null,
-                    data // data es igual a la data que se recibe
-                })
+                
+                if(isMounted.current){
+                    setstate({
+                        loading: false,
+                        error: null,
+                        data // data es igual a la data que se recibe
+                    })
+                }
+
 
             })
         
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 //import SimpleForm from './components/02-useEffect/SimpleForm';
 //import CounterApp from './components/01-useState/CounterApp';
 //import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 //import HookApp from './HookApp';
+
+
 ReactDOM.render(
-    < MultipleCustomHooks />,
+    < RealExampleRef />,
   document.getElementById('root')
 );
